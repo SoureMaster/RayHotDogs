bb4112a740131dfcc68c5acde96f167292b53625
Romney <RomneyJ@gencor.com>
2018-01-30 15:06:07 -0500

    Add project files.

---
+++ RayHotDogs.Core/Model/HotDogGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayHotDogs.Core.Model
+{
+   public class HotDogGroup
+    {
+        public int HotDogGroupID { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public List<HotDogs> HotDogs { get; set; }
+  
+    }
+}
